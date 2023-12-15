e86284fbad8e14e31fb28a87c55801d08584acd2
ahmedamr <ahmed.eslam51a@gmail.com>
2023-12-16 00:36:19 +0200

    finish creating login cubit with states and cubit

---
+++ lib/views/cubits/login_cubit/login_state.dart
@@ -0,0 +1,11 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+final class LoginSuccess extends LoginState {}
+final class LoginFailed extends LoginState {}
+final class LoginLoading extends LoginState {}
+
+
